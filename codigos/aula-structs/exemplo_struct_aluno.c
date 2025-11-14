e895fc2d0b15d88a790906ac7f6b63a82c70f053
Lucas Boaventura <lucasxboaventura@hotmail.com>
2025-11-14 13:59:46 -0300

    Adiciona codigo com a struct aluno

--- codigos/aula-structs/exemplo_struct_aluno.c
+++ codigos/aula-structs/exemplo_struct_aluno.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
-struct aluno {
+typedef struct aluno {
     char nome[101];
     int matricula;
     float notas[3];
-};
+} Aluno;
+
+// typedef struct aluno Aluno;
 
 int main() {
-    struct aluno aluno1 = {
-        .nome = "Maria",
-        .matricula = 66666,
-        .notas = {10.0, 8.5, 6.4}
-    };
+    Aluno alunos[3];
 
-    printf("Matricula: %d\n", aluno1.matricula);
-    printf("Nome: %s\n", aluno1.nome);
+    for(int i = 0; i < 3; i++) {
+        printf("Digite os dados do aluno [%d]\n", i);
+        scanf("%[^\n]", alunos[i].nome);
+        getchar();
+        scanf("%d", &alunos[i].matricula);
+        getchar();
+        alunos[i].notas[0] = 0.0;
+        alunos[i].notas[1] = 0.0;
+        alunos[i].notas[2] = 0.0;
+    }
 
+    for(int i = 0; i < 3; i++) {
+        printf("----------------------\n[%d]\n", i);
+        printf("Nome: %s\n", alunos[i].nome);
+        printf("Matricula: %d\n", alunos[i].matricula);
+        printf("Notas[1]: %.2f\n", alunos[i].notas[0]);
+        printf("Notas[2]: %.2f\n", alunos[i].notas[1]);
+        printf("Notas[3]: %.2f\n", alunos[i].notas[2]);
+    }
+
+    
     return 0;
 }

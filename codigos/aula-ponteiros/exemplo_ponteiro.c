c2479f166c1438b98285affcccc5ce6e54bfa979
Lucas Boaventura <lucasxboaventura@hotmail.com>
2025-11-25 19:19:45 -0300

    adiciona codigo de ponteiros

--- codigos/aula-ponteiros/exemplo_ponteiro.c
+++ codigos/aula-ponteiros/exemplo_ponteiro.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int i = 30;
+    int i = 78;
     int *p = NULL;
     p = &i;
 
     *p = 56;
+    printf("Endereço de i (&i): %p\n", &i);
     printf("Endereço armazenado em p (p = &i): %p\n", p);
+    printf("Valor de i (i): %d\n", i);
     printf("Valor armazenado no endereço de p (*p): %d\n", *p);
 
     return 0;
